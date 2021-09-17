eb09fa79d5ea1c0015b9e052f480b4b73375cd09
deepalics0044 <deepalics0044@gmail.com>
2021-09-17 19:18:18 +0530

    Changes for Pricegradient

--- public/js/ForgeViewer.js
+++ public/js/ForgeViewer.js
@@ -32,4 +32,16 @@ function getForgeToken(callback) {
       callback(data.access_token, data.expires_in);
     });
   });
+ function getAllLeafComponents(callback) {
+    this.viewer.getObjectTree(function (tree) {
+        let leaves = [];
+        tree.enumNodeChildren(tree.getRootId(), function (dbId) {
+            if (tree.getChildCount(dbId) === 0) {
+                leaves.push(dbId);
+            }
+        }, true);
+        callback(leaves);
+    });
+}
+
 }
